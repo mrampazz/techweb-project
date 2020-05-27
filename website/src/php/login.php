aecb011b00d8c8fba5fd3bf52d22bbb684c985c6
Andrea Polo <polo.andrea98@gmail.com>
2020-05-27 14:58:39 +0200

    link login->registrati registrazione->login, modificata gestione erorri

--- website/src/php/login.php
+++ website/src/php/login.php
@@ -4,7 +4,7 @@ include_once("../../server/utils.php");
 $output = file_get_contents("../html/login.html");
 
 if(isset($_SESSION['error-message']) && isset($_SESSION['login']) && !$_SESSION['login']) {
-    $output = str_replace("<div class='margin-top-2 hidden'>","<div class='margin-top-2' tabindex='0'>",$output);
+    $output = str_replace("<div class=\"margin-top-2 hidden\">","<div class=\"margin-top-2\" tabindex=\"0\">",$output);
     $output = str_replace("{error-message}",$_SESSION['error-message'],$output);
 }
 /* check if login session variable is set
